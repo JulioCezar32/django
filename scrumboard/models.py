8ceeba5c504cc42d9c7a688eca67f016dd42f32e
Julio Cezar <jc.2013oliveira@gmai.com>
2018-09-24 00:11:37 -0300

    new features

--- scrumboard/models.py
+++ scrumboard/models.py
@@ -14,7 +14,7 @@ class Card(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     list = models.ForeignKey(List, related_name="cards", on_delete=True)
-    story_poits = models.IntegerField(null=True, blank=True)
+    story_points = models.IntegerField(null=True, blank=True)
     business_value = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
